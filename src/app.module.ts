5efe1e5ee7077a09bca8f5d80b5b093acd8db7f0
Emanuel <emanuelfiuri@fullaudits.com>
2023-04-26 11:27:13 -0300

    servicio and CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicioModule } from './servicio/servicio.module';
 
 @Module({
-  imports: [],
+  imports: [ServicioModule],
   controllers: [AppController],
   providers: [AppService],
 })
